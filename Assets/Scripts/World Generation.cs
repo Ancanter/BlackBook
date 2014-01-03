3fecbb323f244235cd6433a10a0594f79a52f68b
Devyn <>
2014-01-02 22:13:23 -0700

    Creation
    Added the current edition of BlackBook.

---
+++ Assets/Scripts/World Generation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGeneration : MonoBehaviour {
+	public int heightLevel, heightMin, heightMax;
+	public GameObject stone, dirt, type;
+
+	// Use this for initialization
+	void Start () {
+		heightLevel = heightMin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int heightLevel; heightLevel<=heightMax; heightLevel++){
+			//Instantiate(
+		}
+	}
+	void lvl_0(){
+
+	}
+}
